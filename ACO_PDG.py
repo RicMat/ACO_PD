0d312314f47e677dcaec11c31a338feaa8354b18
RicMat <da.ri.mat42@gmail.com>
2020-03-28 17:50:00 +0100

    Update ACO_PDG.py

--- ACO_PDG.py
+++ ACO_PDG.py
@@ -10,32 +10,39 @@ import numpy as np
 
 class ACO_PDG():
     
-    def __init__(self, width, height, obstacles, ant_count, step_count, alpha, beta, gamma, evaporation_rate, start, end):
+    def __init__(self, width, height, obstacles, cycle_num, ant_num, gamma, evaporation_rate, start, end, brushfire_iter):
         self.width = width
         self.height = height
+        self.cycle_num = cycle_num
         self.obstacles = obstacles
-        self.ant_count = ant_count
+        self.ant_num = ant_num
+        self.gamma = gamma
         self.pheromone_grid = np.ones(shape=(self.width, self.height))
         self.evaporation_rate = evaporation_rate
         self.start = start
         self.end = end
+        self.brushfire_iter = brushfire_iter
+        self.heuristic_grid = self.Potential_Field_Heuristic(brushfire_iter)
+        self.ants_movements = []
     
     #local pheromone diffusion model
     
-    def U_att(self, K_a, p, p_e):
-        return 1/2. * K_a * (p-p_e)^2
-    
-    def U_rep(self, K_r, rho, rho_0):
-        if rho <= rho_0:
-            return 0
+    def U_att(self, p, p_e):
+        K_a = 1.
+        return K_a * np.linalg.norm(p - p_e) ** 2 / 2.
+
+    def U_rep(self, rho, rho_o):
+        K_r = 25.
+        if rho <= rho_o:
+            return K_r * (1/rho - 1/rho_o) ** 2 / 2.
         else:
-            return 1/2. * K_r * (1/rho - 1/rho_0)^2
-        
-    def U_total(self, u_att, u_rep):
-        return u_att + u_rep
+            return 0
+
+    def U_total(self, p, p_e, rho, rho_o):
+        return self.U_att(p, p_e) + self.U_rep(rho, rho_o)
     
-    def diff_pheromone(self, gamma, phi, r, xi):
-        return gamma * phi * (r - xi) / r
+    def diff_pheromone(self, phi, r, xi):
+        return self.gamma * phi * (r - xi) / r
     
     # geometry optimization path model
     
@@ -97,20 +104,61 @@ class ACO_PDG():
         for i in range(self.width):
             for j in range(self.height):
                 if sdo[i, j] != 0:
-                    heuristic_grid[i, j] = self.U_tot(np.array([i, j]), self.end, sdo[i, j], 4)
+                    heuristic_grid[i, j] = self.U_total(np.array([i, j]), self.end, sdo[i, j], 4)
         return 5 * (1 - heuristic_grid / np.max(heuristic_grid))
     
-    def algorithm(self): #core
-        d_pheromone = np.zeros(shape=(self.width, self.height)) #step 3
+    def check_available_grids(self, position, previous):
+        x = position[0]
+        y = position[1]
+        x_p = previous[0]
+        y_p = previous[1]
+        grid_list = []
+        for i in range(max(x - 1, 0), min(x + 2, 20)):
+            for j in range(max(y - 1, 0), min(y + 2, 20)):
+                if (not (i, j) == (x_p, y_p)) and (not (i, j) == (x, y)) and (not (i, j) in self.obstacles):
+                    grid_list.append((i, j))
+        return grid_list
+    
+    def algorithm(self, Q): #core
+        for t in range(self.cycle_num):
+            plt.figure()
+            plt.imshow(self.pheromone_grid)
+            plt.show()
+            complete_paths = []
+            tmp_pheromone = np.zeros(shape=(self.width, self.height))
+            for i in range(self.ant_num):
+                path = self.ant_act()
+                if path[-1] == (self.end[0], self.end[1]):
+                    complete_paths.append((self.path_len(path), path)) #step 7
+                    for grid in path: # step 8
+                        tmp_pheromone[grid[0], grid[1]] += Q / self.path_len(complete_paths[-1][1])
+                        for j in range(max(grid[0] - 1, 0), min(grid[0] + 2, 20)):
+                            for k in range(max(grid[1] - 1, 0), min(grid[1] + 2, 20)):
+                                if not (j, k) in self.obstacles:
+                                    self.pheromone_grid[j, k] += self.diff_pheromone(self.pheromone_grid[j, k], 2, np.linalg.norm(np.array(grid) - np.array((j, k))))
+                    path = self.geometry_opt_path(path) #step 9
+                    complete_paths.append((self.path_len(path), path))
+                    for grid in path: # step 10
+                        tmp_pheromone[grid[0], grid[1]] += Q / self.path_len(complete_paths[-1][1])
+                        for j in range(max(grid[0] - 1, 0), min(grid[0] + 2, 20)):
+                            for k in range(max(grid[1] - 1, 0), min(grid[1] + 2, 20)):
+                                if not (j, k) in self.obstacles:
+                                    self.pheromone_grid[j, k] += self.diff_pheromone(self.pheromone_grid[j, k], 2, np.linalg.norm(np.array(grid) - np.array((j, k))))
+                    
+            self.pheromone_grid = (1 - self.evaporation_rate) * self.pheromone_grid  + tmp_pheromone
         
-    def ant_act(self): #movement - step 4 to 6
+        complete_paths = sorted(complete_paths, key=lambda x: x[0])
+        return complete_paths
+                    
+       
+    def ant_act(self): #movement - step 3 to 6
         max_steps = 100
         position = self.start
-        alpha = 12
-        beta = 0.02
+        alpha = 1.1
+        beta = 12
         visited_grids = [(position[0],position[1])]
         for i in range(max_steps): #step 6
-            available_grids = check_available_grids(visited_grids[i], visited_grids[i-1]) #step 5
+            available_grids = self.check_available_grids(visited_grids[i], visited_grids[i-1]) #step 5
             prob = [] #step 4
             for avail in available_grids:
                 prob.append(self.pheromone_grid[avail[0], avail[1]] ** alpha * self.heuristic_grid[avail[0], avail[1]] ** beta)
@@ -122,15 +170,69 @@ class ACO_PDG():
                 if prob[best_j] >= r:
                     break
             visited_grids.append(available_grids[best_j])
+            
             if available_grids[best_j][0] == self.end[0] and available_grids[best_j][1] == self.end[1]:
                 break
         return visited_grids
+    
+    def plot(self, path):
+        table = np.ones(shape=(self.width, self.height))
+        for obstacle in self.obstacles:
+            table[obstacle[0], obstacle[1]] = 0.2
+        for grid in path:
+            table[grid[0], grid[1]] = 0.5
+        plt.figure()
+        plt.imshow(table)
+        plt.show()
+        return table
+    
+    def path_len(self, path):
+        length = 0
+        for i in range(1, len(path)):
+            length += np.linalg.norm(np.array(path[i]) - np.array(path[i-1]))
+        return length
+    
+    def __repr__(self):
+        o = str(self.width) + "x" + str(self.height) + " grid with obstacles:\n"
+        o += str(self.obstacles)
+        return o
         
-            
         
 
 if __name__ == "__main__":
 
     #main
-
+    
+    Obstacles = [(1, 16), 
+                 (2, 2), (2, 7), (2, 8), (2, 16), 
+                 (3, 1), (3, 2), (3, 7), (3, 15), (3, 16), 
+                 (4, 2), (4, 3), (4, 6), (4, 7), (4, 15), (4, 16),
+                 (5, 2), (5, 6), (5, 7), 
+                 (6, 7), (6, 11),
+                 (7, 7), (7, 11), (7, 12),
+                 (8, 11), (8, 12),
+                 (9, 2), (9, 3), (9, 11), (9, 17),
+                 (10, 2), (10, 3), (10, 8), (10, 16), (10, 17),
+                 (11, 2), (11, 7), (11, 8), (11, 16), (11, 17),
+                 (12, 6), (12, 7), (12, 8),
+                 (13, 6), (13, 7), (13, 12), (13, 17),
+                 (14, 4), (14, 6), (14, 12), (14, 16), (14, 17), (14, 18),
+                 (15, 4), (15, 12), (15, 13), (15, 17),
+                 (16, 2), (16, 3), (16, 4), (16, 12), (16, 13), (16, 17),
+                 (17, 3), (17, 4), (17, 12), (17, 13),
+                 (18, 12)]
+    aco_pdg = ACO_PDG(20, 20, Obstacles, 200, 10, 0.01, 0.5, np.array([0, 0]), np.array([19, 19]), 5)
+    heuristic_grid = aco_pdg.heuristic_grid
+    plt.figure()
+    plt.imshow(heuristic_grid)
+    plt.show()
+    path = aco_pdg.ant_act()
+    aco_pdg.plot(path)
+    print("aaa")
+    path = aco_pdg.geometry_opt_path(path)
+    aco_pdg.plot(path)
+    print("bbb")
+    o = aco_pdg.algorithm(10)
+    aco_pdg.plot(o[0][1])
+    print("Shortest path length: " + str(o[0][0]))
 
